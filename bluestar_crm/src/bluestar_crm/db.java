9ba8bd83384e6bc73e38bc72ff66d4fb919fcdd6
abscot <73824086+abscot@users.noreply.github.com>
2021-01-02 10:11:29 +0000

    BlueStar Code Upload and database
    Connection code , login page code and other pages to link up

---
+++ bluestar_crm/src/bluestar_crm/db.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bluestar_crm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author abigail.cotterill
+ */
+public class db {
+    
+        public static Connection Connect() {
+        
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\abigail.cotterill\\OneDrive - Accenture\\College\\Level 6 - LAST YEAR!! You can do this!!\\CMP330 Systems Development\\Practical Assignment\\bluestar_db.db");
+            System.out.println("Connected to database");
+            return conn;
+            
+        }
+        catch (Exception e) {
+            System.out.println("Error: " + e);
+            return null;
+        }
+        
+    }
+}
+
